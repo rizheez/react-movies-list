a2b4c5aca7252c8575eecdd51ee4fdd963dc1ed9
Rizheez <rezamuhammad980@gmail.com>
2025-08-15 15:40:27 +0800

    Add routes for Movie and TV pages in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import Navbar from "@/components/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 
 import Home from "@/pages/Home";
-
+import Movie from "@/pages/Movie";
+import Tv from "@/pages/Tv";
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
@@ -11,6 +12,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movie />} />
+          <Route path="/tv" element={<Tv />} />
           {/* <Route path="/movie/:id" element={MovieDetail} />  */}
         </Routes>
       </Router>
