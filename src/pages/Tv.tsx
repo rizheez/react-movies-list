fe4da1dfd4b9142784b36be9ef2f84b164a45b07
Rizheez <rezamuhammad980@gmail.com>
2025-08-19 15:21:38 +0800

    Implement search functionality in Tv component; fetch TV shows based on search query and update UI accordingly. Adjust loading states and conditionally render sections based on search results.

--- src/pages/Tv.tsx
+++ src/pages/Tv.tsx
@@ -2,74 +2,96 @@ import { useState, useEffect } from "react";
 import type { Categories, Tv, APITvResponse } from "@/types";
 import MovieSection from "@/components/movie-section";
 import api from "@/lib/axios";
-
+import { useSearchParams } from "react-router-dom";
 export default function Tv() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [tv, setTv] = useState<Tv[]>([]);
   const [genres, setGenres] = useState<Categories[]>([]);
   const [tvByCategory, setTvByCategory] = useState<Record<string, Tv[]>>({});
   const [topRated, setTopRated] = useState<Tv[]>([]);
-
+  const [searchParams] = useSearchParams();
+  const querySearch = searchParams.get("search");
   useEffect(() => {
     const fetchTv = async () => {
       try {
-        const { data } = await api.get<{ results: APITvResponse[] }>("/tv/popular", {
-          params: {
-            sort_by: "popularity.desc",
-            page: 1,
-          },
-        });
-
-        setTv(
-          data.results.map((e) => ({
-            poster_path: `https://image.tmdb.org/t/p/w500${e.poster_path}`,
-            name: e.name,
-            id: e.id,
-            overview: e.overview,
-            backdrop_path: `https://image.tmdb.org/t/p/w500${e.backdrop_path}`,
-          }))
-        );
-        const top = await api.get<{ results: APITvResponse[] }>("/tv/top_rated", {
-          params: {
-            page: 1,
-            sort_by: "vote_average.desc",
-            vote_count: {
-              gte: 200,
+        if (querySearch) {
+          const { data } = await api.get<{ results: APITvResponse[] }>("/search/tv", {
+            params: {
+              query: querySearch,
+              page: 1,
             },
-          },
-        });
-        setTopRated(
-          top.data.results.map((e) => ({
-            poster_path: `https://image.tmdb.org/t/p/w500${e.poster_path}`,
-            name: e.name,
-            id: e.id,
-            overview: e.overview,
-            backdrop_path: `https://image.tmdb.org/t/p/w500${e.backdrop_path}`,
-          }))
-        );
-        const genres = await api.get<{ genres: Categories[] }>("/genre/tv/list");
-        setGenres(genres.data.genres);
-        const tvPerCategory: Record<string, Tv[]>[] = await Promise.all(
-          genres.data.genres.map(async (g) => {
-            const { data } = await api.get<{ results: APITvResponse[] }>("/discover/tv", {
-              params: {
-                with_genres: g.id,
-                sort_by: "popularity.desc",
-                page: 1,
+          });
+          setTv(
+            data.results.map((e) => ({
+              poster_path: `https://image.tmdb.org/t/p/w500${e.poster_path}`,
+              name: e.name,
+              id: e.id,
+              overview: e.overview,
+              backdrop_path: `https://image.tmdb.org/t/p/w500${e.backdrop_path}`,
+            }))
+          );
+          setTopRated([]);
+          setGenres([]);
+          setTvByCategory({});
+        } else {
+          const { data } = await api.get<{ results: APITvResponse[] }>("/tv/popular", {
+            params: {
+              sort_by: "popularity.desc",
+              page: 1,
+            },
+          });
+
+          setTv(
+            data.results.map((e) => ({
+              poster_path: `https://image.tmdb.org/t/p/w500${e.poster_path}`,
+              name: e.name,
+              id: e.id,
+              overview: e.overview,
+              backdrop_path: `https://image.tmdb.org/t/p/w500${e.backdrop_path}`,
+            }))
+          );
+          const top = await api.get<{ results: APITvResponse[] }>("/tv/top_rated", {
+            params: {
+              page: 1,
+              sort_by: "vote_average.desc",
+              vote_count: {
+                gte: 200,
               },
-            });
-            return {
-              [g.name]: data.results.map((e) => ({
-                poster_path: `https://image.tmdb.org/t/p/w500${e.poster_path}`,
-                name: e.name,
-                id: e.id,
-                overview: e.overview,
-                backdrop_path: `https://image.tmdb.org/t/p/w500${e.backdrop_path}`,
-              })),
-            };
-          })
-        );
-        setTvByCategory(tvPerCategory.reduce((acc, curr) => ({ ...acc, ...curr }), {}));
+            },
+          });
+          setTopRated(
+            top.data.results.map((e) => ({
+              poster_path: `https://image.tmdb.org/t/p/w500${e.poster_path}`,
+              name: e.name,
+              id: e.id,
+              overview: e.overview,
+              backdrop_path: `https://image.tmdb.org/t/p/w500${e.backdrop_path}`,
+            }))
+          );
+          const genres = await api.get<{ genres: Categories[] }>("/genre/tv/list");
+          setGenres(genres.data.genres);
+          const tvPerCategory: Record<string, Tv[]>[] = await Promise.all(
+            genres.data.genres.map(async (g) => {
+              const { data } = await api.get<{ results: APITvResponse[] }>("/discover/tv", {
+                params: {
+                  with_genres: g.id,
+                  sort_by: "popularity.desc",
+                  page: 1,
+                },
+              });
+              return {
+                [g.name]: data.results.map((e) => ({
+                  poster_path: `https://image.tmdb.org/t/p/w500${e.poster_path}`,
+                  name: e.name,
+                  id: e.id,
+                  overview: e.overview,
+                  backdrop_path: `https://image.tmdb.org/t/p/w500${e.backdrop_path}`,
+                })),
+              };
+            })
+          );
+          setTvByCategory(tvPerCategory.reduce((acc, curr) => ({ ...acc, ...curr }), {}));
+        }
       } catch (error) {
         console.error("Error fetching tv:", error);
       } finally {
@@ -78,20 +100,30 @@ export default function Tv() {
     };
 
     fetchTv();
-  }, []);
+  }, [querySearch]);
   return (
     <>
       <div className="container mx-auto my-8 py-8">
-        <MovieSection title="Trending" movies={tv} isLoading={isLoading} />
-        <MovieSection title="Top Rated" movies={topRated} isLoading={isLoading} />
-        {genres.map((g) => (
-          <MovieSection
-            key={g.id}
-            title={g.name}
-            movies={tvByCategory[g.name]}
-            isLoading={isLoading}
-          />
-        ))}
+        <MovieSection
+          title={querySearch ? `Search for "${querySearch}"` : "Trending Tv"}
+          movies={tv}
+          isLoading={isLoading}
+        />
+        {!tv.length && <h1 className="text-center text-2xl font-bold">No results found</h1>}
+
+        {!querySearch && (
+          <>
+            <MovieSection title="Top Rated" movies={topRated} isLoading={isLoading} />
+            {genres.map((g) => (
+              <MovieSection
+                key={g.id}
+                title={g.name}
+                movies={tvByCategory[g.name]}
+                isLoading={isLoading}
+              />
+            ))}
+          </>
+        )}
       </div>
     </>
   );
